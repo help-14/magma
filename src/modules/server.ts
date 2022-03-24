413704cc142d220846d031222f5ee29b3a1accd0
Help-14 <help-14@hotmail.com>
2022-03-24 23:41:28 +0700

    stack bookmark layout is 50% done

--- src/modules/server.ts
+++ src/modules/server.ts
@@ -1,30 +1,27 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { opine, serveStatic } from "https://deno.land/x/opine/mod.ts";
 import { ensureDirSync, existsSync } from "https://deno.land/std/fs/mod.ts";
 import { Language, minify } from "https://deno.land/x/minifier/mod.ts";
 
 const tempFolder = `${Deno.cwd()}/temp`;
 
 export async function startServer(): Promise<void> {
-  const router = new Router();
-  router
-    .get("/", (context) => {
-      context.response.body = window.index;
+  const app = opine();
+  app.use(serveStatic("./public"));
+  app.get("/", function (req, res) {
+    res.headers = new Headers({
+      "content-type": "text/html; charset=UTF-8",
     });
-
-  const app = new Application();
-  app.use(router.routes());
-  app.use(router.allowedMethods());
-  app.use(async (context, next) => {
-    try {
-      await context.send({
-        root: `${Deno.cwd()}/public`,
-      });
-    } catch {
-      next();
-    }
+    res.send(window.index);
   });
 
-  await app.listen({ port: window.config.server.port });
+  Compile();
+  app.listen(
+    window.config.server.port,
+    () =>
+      console.log(
+        `server has started on http://localhost:${window.config.server.port} 🚀`,
+      ),
+  );
 }
 
 export async function Compile(): Promise<void> {
@@ -54,15 +51,26 @@ export async function CompileIndex(): Promise<void> {
   }
 
   //Minify HTML
-  indexContent = minify(Language.HTML, indexContent);
+  if (window.config.server.minify) {
+    indexContent = minify(Language.HTML, indexContent);
+  }
 
   //Write index.html into temp folder
   ensureDirSync(tempFolder);
-  await Deno.writeTextFile(`${tempFolder}/index.html`, indexContent);
+  window.index = indexContent;
+  //await Deno.writeTextFile(`${tempFolder}/index.html`, indexContent);
 }
 
 export async function CompileCSS(path: string): Promise<void> {
+  if (!path) {
+  }
+
+  if (window.config.server.minify) {}
 }
 
 export async function CompileJS(path: string): Promise<void> {
+  if (!path) {
+  }
+
+  if (window.config.server.minify) {}
 }
