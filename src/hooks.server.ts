b360534a7f5676c57f94404a4bdbc66e383d024c
help-14 <help-14@hotmail.com>
2026-07-05 09:42:41 +0700

    feat: add auth handle hook to check session cookie

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,18 @@
 import { paraglideMiddleware } from '$lib/paraglide/server';
+import { verifySession } from '$lib/server/auth.js';
 import type { Handle } from '@sveltejs/kit';
 
-const paraglideHandle: Handle = ({ event, resolve }) =>
-	paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
-		event.request = localizedRequest;
-		return resolve(event, {
-			transformPageChunk: ({ html }) => {
-				return html.replace('%lang%', locale).replace('%dir%', 'ltr');
-			}
-		});
-	});
-
-export const handle = paraglideHandle;
+export const handle: Handle = ({ event, resolve }) => {
+	event.locals.isAuthenticated = verifySession(event.cookies);
+	return paraglideMiddleware(
+		event.request,
+		({ request: localizedRequest, locale }) => {
+			event.request = localizedRequest;
+			return resolve(event, {
+				transformPageChunk: ({ html }) => {
+					return html.replace('%lang%', locale).replace('%dir%', 'ltr');
+				}
+			});
+		}
+	);
+};
